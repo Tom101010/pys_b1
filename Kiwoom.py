0dc1917ad8ca5947063ce1253335884fb9853a34
Tom1010 <marks110010@gmail.com>
2020-06-05 23:39:14 +0900

    200605_ch18

--- Kiwoom.py
+++ Kiwoom.py
@@ -21,6 +21,7 @@ class Kiwoom(QAxWidget):
     def _set_signal_slots(self):
         self.OnEventConnect.connect(self._event_connect)
         self.OnReceiveTrData.connect(self._receive_tr_data)
+        self.OnReceiveChejanData.connect(self._receive_chejan_data)
 
     def comm_connect(self):
         self.dynamicCall("CommConnect()")
@@ -77,6 +78,10 @@ class Kiwoom(QAxWidget):
         ret = self.dynamicCall("GetChejanData(int)", fid)
         return ret
 
+    def get_server_gubun(self):
+        ret = self.dynamicCall("KOA_Functions(QString, QString)", "GetServerGubun", "")
+        return ret
+
     def _receive_chejan_data(self, gubun, item_cnt, fid_list):
         print(gubun)
         print(self.get_chejan_data(9203))
@@ -94,7 +99,6 @@ class Kiwoom(QAxWidget):
             self._opt10081(rqname, trcode)
         elif rqname == "opw00001_req":
             self._opw00001(rqname, trcode)
-
         if rqname == "opt10081_req":
             self._opt10081(rqname, trcode)
 
@@ -102,6 +106,36 @@ class Kiwoom(QAxWidget):
             self.tr_event_loop.exit()
         except AttributeError:
             pass
+
+    @staticmethod
+    def change_format(data):
+        strip_data = data.lstrip('-0')
+        if strip_data == '' or strip_data == '.00':
+            strip_data = '0'
+
+        try:
+            format_data = format(int(strip_data), ',d')
+        except:
+            format_data = format(float(strip_data))
+        if data.startswith('-'):
+            format_data = '-' + format_data
+
+        return format_data
+
+    @staticmethod
+    def change_format2(data):
+        strip_data = data.lstrip('-0')
+
+        if strip_data == '':
+            strip_data = '0'
+
+        if strip_data.startswith('.'):
+            strip_data = '0' + strip_data
+
+        if data.startswith('-'):
+            strip_data = '-' + strip_data
+
+        return strip_data
 
     def _opw00001(self, rqname, trcode):
         self.d2_deposit = self._comm_get_data(trcode, "", rqname, 0, "d+2추정예수금")
@@ -125,26 +159,60 @@ class Kiwoom(QAxWidget):
             self.ohlcv['close'].append(int(close))
             self.ohlcv['volume'].append(int(volume))
 
+    def reset_opw00018_output(self):
+        self.opw00018_output = {'single': [], 'multi': []}
+
+    def _opw00018(self, rqname, trcode):
+        # single data
+        total_purchase_price = self._comm_get_data(trcode, "", rqname, 0, "총매입금액")
+        total_eval_price = self._comm_get_data(trcode, "", rqname, 0, "총평가금액")
+        total_eval_profit_loss_price = self._comm_get_data(trcode, "", rqname, 0, "총평가손익금액")
+        total_earning_rate = self._comm_get_data(trcode, "", rqname, 0, "총수익률(%)")
+        estimated_deposit = self._comm_get_data(trcode, "", rqname, 0, "추정예탁자산")
+
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_purchase_price))
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_eval_price))
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_eval_profit_loss_price))
+
+        total_earning_rate = Kiwoom.change_format(total_earning_rate)
+
+        if self.get_server_gubun():
+            total_earning_rate = float(total_earning_rate) / 100
+            total_earning_rate = str(total_earning_rate)
+
+        self.opw00018_output['single'].append(total_earning_rate)
+
+        self.opw00018_output['single'].append(Kiwoom.change_format(estimated_deposit))
+
+        # multi data
+        rows = self._get_repeat_cnt(trcode, rqname)
+        for i in range(rows):
+            name = self._comm_get_data(trcode, "", rqname, i, "종목명")
+            quantity = self._comm_get_data(trcode, "", rqname, i, "보유수량")
+            purchase_price = self._comm_get_data(trcode, "", rqname, i, "매입가")
+            current_price = self._comm_get_data(trcode, "", rqname, i, "현재가")
+            eval_profit_loss_price = self._comm_get_data(trcode, "", rqname, i, "평가손익")
+            earning_rate = self._comm_get_data(trcode, "", rqname, i, "수익률(%)")
+
+            quantity = Kiwoom.change_format(quantity)
+            purchase_price = Kiwoom.change_format(purchase_price)
+            current_price = Kiwoom.change_format(current_price)
+            eval_profit_loss_price = Kiwoom.change_format(eval_profit_loss_price)
+            earning_rate = Kiwoom.change_format2(earning_rate)
+
+            self.opw00018_output['multi'].append([name, quantity, purchase_price, current_price, eval_profit_loss_price,
+                                                  earning_rate])
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     kiwoom = Kiwoom()
     kiwoom.comm_connect()
-    kiwoom.ohlcv = {'date': [], 'open': [], 'high': [], 'low': [], 'close': [], 'volume': []}
 
-    # opt10081 TR 요청
-    kiwoom.set_input_value("종목코드", "039490")
-    kiwoom.set_input_value("기준일자", "20170224")
-    kiwoom.set_input_value("수정주가구분", 1)
-    kiwoom.comm_rq_data("opt10081_req", "opt10081", 0, "0101")
+    kiwoom.reset_opw00018_output()
+    account_number = kiwoom.get_login_info("ACCNO")
+    account_number = account_number.split(';')[0]
 
-    while kiwoom.remained_data == True:
-        time.sleep(TR_REQ_TIME_INTERVAL)
-        kiwoom.set_input_value("종목코드", "039490")
-        kiwoom.set_input_value("기준일자", "20170224")
-        kiwoom.set_input_value("수정주가구분", 1)
-        kiwoom.comm_rq_data("opt10081_req", "opt10081", 2, "0101")
-
-    df = pd.DataFrame(kiwoom.ohlcv, columns=['open', 'high', 'low', 'close', 'volume'], index=kiwoom.ohlcv['date'])
-
-    con = sqlite3.connect("c:/Users/Jason/stock.db")
-    df.to_sql('039490', con, if_exists='replace')
+    kiwoom.set_input_value("계좌번호", account_number)
+    kiwoom.comm_rq_data("opw00018_req", "opw00018", 0, "2000")
+    print(kiwoom.opw00018_output['single'])
+    print(kiwoom.opw00018_output['multi'])
